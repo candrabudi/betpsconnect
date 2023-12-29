90d8fb0a6249ed99fc084592b266a587ee5fe4d0
candrabudi <candrasachio@gmail.com>
2023-12-29 14:36:23 +0700

    middleware on router

--- internal/app/district/router.go
+++ internal/app/district/router.go
@@ -1,9 +1,12 @@
 package district
 
 import (
+	"betpsconnect/internal/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (h *handler) Router(g *gin.RouterGroup) {
+	g.Use(middleware.Authenticate())
 	g.GET("/by-city", h.GetDistrictByCity)
 }
--- internal/app/subdistrict/router.go
+++ internal/app/subdistrict/router.go
@@ -1,9 +1,12 @@
 package subdistrict
 
 import (
+	"betpsconnect/internal/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (h *handler) Router(g *gin.RouterGroup) {
+	g.Use(middleware.Authenticate())
 	g.GET("/by-district", h.GetByDistrict)
 }
