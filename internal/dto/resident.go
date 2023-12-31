a4aa4f2a75e5c69e7097cce1202e5f5a67570b12
candrabudi <candrasachio@gmail.com>
2023-12-31 14:39:07 +0700

    fix response list resident

--- internal/dto/resident.go
+++ internal/dto/resident.go
@@ -44,6 +44,9 @@ type (
 		NamaKabupaten string `bson:"nama_kabupaten,omitempty" json:"nama_kabupaten"`
 		NamaKecamatan string `bson:"nama_kecamatan,omitempty" json:"nama_kecamatan"`
 		Nik           string `bson:"nik,omitempty" json:"nik"`
+		TanggalLahir  string `bson:"tanggal_lahir,omitempty" json:"tanggal_lahir"`
+		Usia          string `bson:"usia,omitempty" json:"usia"`
+		Tps           string `bson:"tps,omitempty" json:"tps"`
 		Status        string `bson:"status,omitempty" json:"status"`
 		IsTrue        int    `bson:"is_true,omitempty" json:"is_true"`
 		IsFalse       int    `bson:"is_false,omitempty" json:"is_false"`
